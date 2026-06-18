3aad8945c04d8243a1d01b86b91d230cf917907d
umino1973 <umino1973@libero.it>
2026-06-18 08:23:11 +0200

    Add bandi.js with funding opportunities data

---
+++ netlify/functions/bandi.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    name: "Smart&Start Italia",
+    entity: "Invitalia",
+    sectors: ["ai","tech","digital","servizi"],
+    stages: ["idea","mvp","startup"],
+    regions: ["italy"],
+    min_capital: 0,
+    max_capital: 1500000,
+    requirements: [
+      "Startup innovativa",
+      "Sede in Italia",
+      "Progetto innovativo"
+    ]
+  },
+  {
+    name: "Fondo Lombardia Start",
+    entity: "Regione Lombardia",
+    sectors: ["servizi","innovazione"],
+    stages: ["idea","mvp"],
+    regions: ["lombardia"],
+    min_capital: 5000,
+    max_capital: 100000,
+    requirements: [
+      "Sede Lombardia",
+      "Early stage",
+      "PMI o startup"
+    ]
+  },
+  {
+    name: "Horizon Europe",
+    entity: "European Commission",
+    sectors: ["ai","deeptech"],
+    stages: ["startup"],
+    regions: ["eu"],
+    min_capital: 0,
+    max_capital: 9999999,
+    requirements: [
+      "Scalabilità UE",
+      "Innovazione profonda",
+      "Team strutturato"
+    ]
+  }
+];
